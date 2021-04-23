907f497d4393e406a8c64ed620a81c48519d609f
bluninjadragon <jwhbootcamp@gmail.com>
2021-04-22 21:46:28 -0500

    scotts updates

---
+++ assets/js/questions.js
@@ -0,0 +1,5 @@
+let questions = [{
+    title: "what color is the sun?",
+    choices: ["yellow", "hot pink", "brown", "lemon chiffon"],
+    answer: ["lemon chiffon"],
+}];
--- assets/js/scottstips.js
+++ assets/js/scottstips.js
@@ -1,6 +1,10 @@
 //define variables////////////
 
 let startBtn = document.querySelector('#start');
+let saveScore = document.querySelector('#save-score');
+let questionTitle = document.querySelector('#question-title');
+let choicesDiv = document.querySelector('#choices');
+let questionIndex = 0;
 
 
 //functions////
@@ -14,19 +18,30 @@ function startQuiz() {
 //get the next question
 function getQuestion() {
     // gets current question
+    let currentQuestion = questions[0];
     // show the question
-    // show the choices with buttons
+    questionTitle.textContent = currentQuestion.title;
+    // show the choices with buttons. instructor taught us syntax for "forEach"
+    currentQuestion.choices.forEach(choice => {
+        let choiceButton = document.createElement("button");
+        choiceButton.textContent = choice;
+        choiceButton.setAttribute("value",choice);
         // add event listener for each button created
+        choiceButton.onclick = answerCheck;
+        choicesDiv.appendChild(choiceButton);
+    });
+        
     answerCheck();
 }
 
 //check user selection
 
 function answerCheck() {
+    alert(this.value);
     //check the user selection against correct answer
     // incorrect selection remove seconds
     // set store score
-    getQuestion();
+    //getQuestion();
     // if questions.length
     endGame();
 }
